55dd601fc922a875239db5b8c84eaf57756e25c0
Maximo Mayo <108980824+MaximoMayo@users.noreply.github.com>
2024-04-18 11:47:54 -0700

    Adding calculator.html and respective JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 /* Maximo Mayo 3/28/2024 */
+
+function calc()
+{
+    /* Getting var for function */
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+
+    /*Var to return answer*/
+    var calculate;
+
+    /*Determine operator; execute command*/
+    if(op == "add")
+    {
+        calculate = a + b;
+    }
+    else if(op == "min")
+    {
+       calculate = a - b; 
+    }
+    else if(op == "div")
+    {
+       calculate = a / b; 
+    }
+    else if(op == "mul")
+    {
+       calculate = a * b; 
+    }
+    else if(op == "mod")
+    {
+       calculate = a % b; 
+    }
+
+    /* Send result back to the HTML to the result id */
+    document.querySelector("#result").innerHTML = calculate;
+}
